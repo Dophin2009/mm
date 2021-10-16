8ef88bded667a7334a7542cc0dd6d7ab89dd7d4c
mgozon <mchinggozon@gmail.com>
2021-10-16 18:57:28 -0400

    added sheet music even more

--- src/components/Sheet.js
+++ src/components/Sheet.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import playIcon from '../assets/play.png';
 import abcjs from 'abcjs';
-import { generateSheet, generateTestSheet } from '../internal/abc';
+import { generateSheet, generateTestSheet, generateTestSheet2, renderSheet, testAudio } from '../internal/abc';
 
 function Sheet() {
     return (
         <div>
-            <button className="bg-red-100" onClick={() => abcjs.renderAbc("paper", generateTestSheet())}>
+            <button className="bg-red-100" onClick={renderSheet}>
                 render abc
+            </button>
+            <button className="bg-blue-100" onClick={testAudio}>
+                test audio
             </button>
             <div className="w-full bg-green-100 flex flex-col items-center" id="paper"></div>
         </div>
